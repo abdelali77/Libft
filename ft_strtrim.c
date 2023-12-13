ca3cf19d5ac2d2040d2e3eb19e605b24dc297577
Abdelali Mahfoudi <abmahfou@c2r8p3.1337.ma>
2023-12-13 18:13:14 +0100

    push

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: abmahfou <abmahfou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/13 10:48:37 by abmahfou          #+#    #+#             */
-/*   Updated: 2023/12/13 12:48:45 by abmahfou         ###   ########.fr       */
+/*   Updated: 2023/12/13 18:09:57 by abmahfou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,41 +18,28 @@ char	*ft_strtrim(char const *s1, char const *set)
 	int		i;
 	int		j;
 	int		n;
+	int		h;
 
 	if (!s1)
 		return (NULL);
+	n = ft_strlen(s1);
 	i = 0;
-	str_trim = malloc(strlen(s1) + 1);
+	j = 0;
+	while (ft_strchr(set, *(s1 + i)) != NULL)
+		i++;
+	while (ft_strchr(set, *(s1 + n)) != NULL)
+		n--;
+	str_trim = malloc(n - i + 1);
 	if (str_trim == NULL)
 		return (NULL);
-	j = strlen(set);
-	while (*(s1 + i) == *(set + i))
+	h = i;
+	while (n - h >= 0)
+	{
+		*(str_trim + j) = *(s1 + i);
 		i++;
-	if (i == j)
-	{
-		i = 0;
-		n = 0;
-		while (*(s1 + j + n))
-		{
-			while (*(s1 + j + n) && *(s1 + j + n) == *(set + n))
-				n++;
-			*(str_trim + i) = *(s1 + j + n);
-			j++;
-			i++;
-		}
+		j++;
+		n--;
 	}
-	else
-	{
-		n = 0;
-		i = 0;
-		while (*(s1 + i))
-		{
-			while (*(s1 + i + n) && *(s1 + i + n) == *(set + n))
-				n++;
-			*(str_trim + i) = *(s1 + i + n);
-			i++;
-		}
-	}
-	*(str_trim + i) = '\0';
+	*(str_trim + j) = '\0';
 	return (str_trim);
 }
