ae85db5b32ed8fe76531e97a6f97899bdf5b459c
Abdelali Mahfoudi <abmahfou@c2r8p3.1337.ma>
2023-12-13 20:05:55 +0100

    push

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: abmahfou <abmahfou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/13 10:48:37 by abmahfou          #+#    #+#             */
-/*   Updated: 2023/12/13 18:09:57 by abmahfou         ###   ########.fr       */
+/*   Updated: 2023/12/13 19:57:56 by abmahfou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,27 +18,32 @@ char	*ft_strtrim(char const *s1, char const *set)
 	int		i;
 	int		j;
 	int		n;
-	int		h;
 
-	if (!s1)
+	if (!s1 || !set)
 		return (NULL);
 	n = ft_strlen(s1);
 	i = 0;
 	j = 0;
-	while (ft_strchr(set, *(s1 + i)) != NULL)
+	while (*(s1 + i) && ft_strchr(set, *(s1 + i)) != NULL)
 		i++;
-	while (ft_strchr(set, *(s1 + n)) != NULL)
+	if (i == n || (i == 0 && ft_strlen(set) == 0))
+	{
+		str_trim = malloc(1);
+		*(str_trim + 0) = '\0';
+		if (str_trim == NULL)
+			return (NULL);
+		return (str_trim);
+	}
+	while (*(s1 + i) && ft_strchr(set, *(s1 + n - 1)) != NULL)
 		n--;
 	str_trim = malloc(n - i + 1);
 	if (str_trim == NULL)
 		return (NULL);
-	h = i;
-	while (n - h >= 0)
+	while (i < n)
 	{
 		*(str_trim + j) = *(s1 + i);
 		i++;
 		j++;
-		n--;
 	}
 	*(str_trim + j) = '\0';
 	return (str_trim);
