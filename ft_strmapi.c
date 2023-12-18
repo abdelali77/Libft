264547bf55dccdf67d0e57ef9542adbadfde930f
Abdelali <abde69436@gmail.com>
2023-12-18 22:32:57 +0100

    main

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,7 +6,7 @@
 /*   By: abmahfou <abmahfou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/14 21:44:19 by abmahfou          #+#    #+#             */
-/*   Updated: 2023/12/14 22:48:45 by abmahfou         ###   ########.fr       */
+/*   Updated: 2023/12/17 11:49:23 by abmahfou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,20 @@
 
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
-	
+	unsigned int	i;
+	char			*str;
+
+	if (!s)
+		return (NULL);
+	i = 0;
+	str = malloc((ft_strlen(s) + 1) * sizeof(char));
+	if (str == NULL)
+		return (NULL);
+	while (*(s + i))
+	{
+		*(str + i) = (*f)(i, *(s + i));
+		i++;
+	}
+	*(str + i) = '\0';
+	return (str);
 }
