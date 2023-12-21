f1f75cc548d922b4cdc3ebb7e006dd11a88f3949
Abdelali <abde69436@gmail.com>
2023-12-21 19:21:27 +0100

    main

---
+++ ft_lstclear_bonus.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear_bonus.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abmahfou <abmahfou@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/21 15:13:37 by abmahfou          #+#    #+#             */
+/*   Updated: 2023/12/21 19:08:29 by abmahfou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstclear(t_list **lst, void (*del)(void *))
+{
+	t_list	*tmp;
+
+	while (*lst != NULL)
+	{
+		tmp = *lst;
+		*lst = (*lst)->next;
+		del(tmp->content);
+		free(tmp);
+	}
+	*lst = NULL;
+}
