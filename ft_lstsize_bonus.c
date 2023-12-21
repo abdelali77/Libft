878baa3c6304483a68bd2c43aee0a5dd16fd528a
Abdelali <abde69436@gmail.com>
2023-12-21 19:22:50 +0100

    main

---
+++ ft_lstsize_bonus.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstsize_bonus.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abmahfou <abmahfou@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/20 12:33:11 by abmahfou          #+#    #+#             */
+/*   Updated: 2023/12/21 10:03:58 by abmahfou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_lstsize(t_list *lst)
+{
+	int	counter;
+
+	counter = 0;
+	while (lst != NULL)
+	{
+		lst = lst->next;
+		counter++; 
+	}
+	return (counter);
+}
