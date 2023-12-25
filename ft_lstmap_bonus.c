9b4fd32738c30d37ef5c287187718b16855e9742
Abdelali <abde69436@gmail.com>
2023-12-25 15:17:05 +0100

    main

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -6,7 +6,7 @@
 /*   By: abmahfou <abmahfou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/21 21:05:00 by abmahfou          #+#    #+#             */
-/*   Updated: 2023/12/21 23:49:27 by abmahfou         ###   ########.fr       */
+/*   Updated: 2023/12/24 21:30:41 by abmahfou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,28 @@
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	t_list	*new_list;
+	t_list	*n_list;
+	t_list	*n_node;
+	void	*c;
 
-	new_list = malloc(sizeof(t_list));
+	n_list = NULL;
 	while (lst != NULL)
 	{
-		f(lst->content);
+		c = f(lst->content);
+		if (!c)
+		{
+			ft_lstclear(&n_list, del);
+			return (NULL);
+		}
+		n_node = ft_lstnew(c);
+		if (!n_node)
+		{
+			del(c);
+			ft_lstclear(&n_list, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&n_list, n_node);
 		lst = lst->next;
 	}
-	new_list = lst;
-	return (new_list);
+	return (n_list);
 }
