75c80ca7775b44c82dec2c8f8151b8df5c23b9d1
Abdelali <abde69436@gmail.com>
2023-12-28 11:45:26 +0100

    main

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: abmahfou <abmahfou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/12 10:41:17 by abmahfou          #+#    #+#             */
-/*   Updated: 2023/12/13 10:17:22 by abmahfou         ###   ########.fr       */
+/*   Updated: 2023/12/26 13:10:36 by abmahfou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,24 +19,19 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	int		s2_len;
 	int		i;
 
+	if (!s1 && !s2)
+		return (NULL);
+	if (!s1)
+		return (ft_strdup(s2));
+	if (!s2)
+		return (ft_strdup(s1));
 	i = 0;
 	s1_len = ft_strlen(s1);
 	s2_len = ft_strlen(s2);
 	str = malloc((s1_len + s2_len) * sizeof(char) + 1);
 	if (str == NULL)
 		return (0);
-	while (*(s1 + i))
-	{
-		*(str + i) = *(s1 + i);
-		i++;
-	}
-	i = 0;
-	while (*(s2 + i))
-	{
-		*(str + s1_len) = *(s2 + i);
-		s1_len++;
-		i++;
-	}
-	*(str + s1_len) = '\0';
+	ft_strlcpy(str, s1, s1_len + 1);
+	ft_strlcat(str, s2, s1_len + s2_len + 1);
 	return (str);
 }
