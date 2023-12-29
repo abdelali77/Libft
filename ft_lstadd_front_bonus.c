ae04b90234cc227f37063d6dde46fbf30846b148
Abdelali <abde69436@gmail.com>
2023-12-29 15:33:26 +0100

    main

--- ft_lstadd_front_bonus.c
+++ ft_lstadd_front_bonus.c
@@ -6,7 +6,7 @@
 /*   By: abmahfou <abmahfou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/19 12:16:47 by abmahfou          #+#    #+#             */
-/*   Updated: 2023/12/27 16:36:05 by abmahfou         ###   ########.fr       */
+/*   Updated: 2023/12/28 13:21:35 by abmahfou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,9 @@
 
 void	ft_lstadd_front(t_list **lst, t_list *new)
 {
-	if (!lst || !new)
-		return ;
-	new->next = *lst;
-	*lst = new;
+	if (lst && new)
+	{
+		new->next = *lst;
+		*lst = new;
+	}
 }
