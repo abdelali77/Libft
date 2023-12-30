5081339a494072bd7e012cf9df814e852b1acac5
Abdelali <abde69436@gmail.com>
2023-12-30 10:36:08 +0100

    main

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: abmahfou <abmahfou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/05 10:53:32 by abmahfou          #+#    #+#             */
-/*   Updated: 2023/12/07 13:08:10 by abmahfou         ###   ########.fr       */
+/*   Updated: 2023/12/26 12:14:32 by abmahfou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,11 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 	i = 0;
 	if (dstsize == 0)
 		return (ft_strlen(src));
-	while (src[i] && i < dstsize - 1)
+	while (*(src + i) && i < dstsize - 1)
 	{
-		dst[i] = src[i];
+		*(dst + i) = *(src + i);
 		i++;
 	}
-	dst[i] = '\0';
+	*(dst + i) = '\0';
 	return (ft_strlen(src));
 }
